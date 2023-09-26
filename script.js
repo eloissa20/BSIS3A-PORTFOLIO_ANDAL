fe50a0c500f84ad091d2376ad7b535a42759b241
Eloissa <mariaeloissa.andal@tup.edu.ph>
2023-09-26 15:31:59 +0800

    NEW FEATURES

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' })
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Blogger'],
+    strings: ['Frontend Developer', 'UI Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
