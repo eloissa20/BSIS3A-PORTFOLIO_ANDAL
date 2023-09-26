5fb8e424f804ab6a5a4f6a89265875552811bb10
Eloissa <mariaeloissa.andal@tup.edu.ph>
2023-09-26 12:36:48 +0800

    ADD NEW CONTENTS AND FIXED

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*scroll activation link*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,10 +26,37 @@ window.onscroll = () => {
         };
     });
 
+    /*sticky navbar*/
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+    /*removing the toglle icon and navbar when you click navbar link (scroll)*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
 
 
+/*scroll reveal*/
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .experience-container, .projects-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' })
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
